76ca1dee5e11b4284ca9348f4da054fa7e1225be
Richelle Xavier <richellexavier@Richelles-MBP.home>
2022-08-27 23:28:37 +0100

    Working on submit btn

--- src/Screens/Measurements.js
+++ src/Screens/Measurements.js
@@ -6,70 +6,56 @@ const Measurements = () => {
     <>
       <Navbar />
       <form>
-        <h1>Client Measurements</h1>
+        <h1>Client's First Measurements</h1>
 
         <div className="txtb">
-          <label>What are your main goals for this exercise program?</label>
-          <input type="radio" value="Option 1" /> Option 1
-          <input type="radio" value="Option 2" /> Option 2
-          <input type="radio" value="Option 3" /> Option 3
-          <input type="radio" value="Option 4" /> Option 4
-          <input type="radio" value="Option 5" /> Option 5
+          <label>Height</label>
+          <input type="text" name="" />
         </div>
 
         <div className="txtb">
-          <label>Which body parts would you like to focus on?</label>
-          <input type="radio" value="Option 1" /> Option 1
-          <input type="radio" value="Option 2" /> Option 2
-          <input type="radio" value="Option 3" /> Option 3
-          <input type="radio" value="Option 4" /> Option 4
-          <input type="radio" value="Option 5" /> Option 5
+          <label>Weight</label>
+          <input type="text" name="" />
         </div>
 
         <div className="txtb">
-          <label>
-            How long after beginning your training regime do you expect to see
-            results?
-          </label>
-          <input type="radio" value="Option 1" /> Option 1
-          <input type="radio" value="Option 2" /> Option 2
-          <input type="radio" value="Option 3" /> Option 3
-          <input type="radio" value="Option 4" /> Option 4
-          <input type="radio" value="Option 5" /> Option 5
+          <label>Body Fat</label>
+          <input type="text" name="" />
         </div>
 
         <div className="txtb">
-          <label>
-            On a scale of 1-10, how would you describe your current knowledge of
-            exercise and fitness?
-          </label>
-          <input type="radio" value="Option 1" /> 1
-          <input type="radio" value="Option 2" /> 2
-          <input type="radio" value="Option 3" /> 3
-          <input type="radio" value="Option 4" /> 4
-          <input type="radio" value="Option 5" /> 5
-          <input type="radio" value="Option 6" /> 6
-          <input type="radio" value="Option 7" /> 7
-          <input type="radio" value="Option 8" /> 8
-          <input type="radio" value="Option 9" /> 9
-          <input type="radio" value="Option 10" /> 10
+          <label>Water</label>
+          <input type="text" name="" />
         </div>
 
         <div className="txtb">
-          <label>What will motivate you to achieve your goals?</label>
-          <input type="text" name="" required />
+          <label>Muscle</label>
+          <input type="text" name="" />
         </div>
 
         <div className="txtb">
-          <label>
-            What are your expected barriers towards your exercise program?
-          </label>
-          <input type="text" name="" required />
+          <label>Physique</label>
+          <input type="text" name="" />
         </div>
 
         <div className="txtb">
-          <label>How can we help you?</label>
-          <input type="text" name="" required />
+          <label>Metabolic Rate</label>
+          <input type="text" name="" />
+        </div>
+
+        <div className="txtb">
+          <label>Metabolic Age</label>
+          <input type="text" name="" />
+        </div>
+
+        <div className="txtb">
+          <label>Bone Mass</label>
+          <input type="text" name="" />
+        </div>
+
+        <div className="txtb">
+          <label>Visceral Fat</label>
+          <input type="text" name="" />
         </div>
 
         <div className="button-div">
@@ -78,9 +64,8 @@ const Measurements = () => {
             <div className="btn-subtext">Agreement</div>
           </a>
 
-          <a className="button" href="/health">
-            <div className="btn-text">Next</div>
-            <div className="btn-subtext">Client Health</div>
+          <a className="button" href="/agreement">
+            <div className="submit-btn-text">Submit</div>
           </a>
         </div>
       </form>
