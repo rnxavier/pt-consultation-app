066c85956c7d0d75de9fc869ae3769dee7c68643
Richelle Xavier <richellexavier@Richelles-MBP.home>
2022-08-28 23:44:39 +0100

    Working on multi part form

---
+++ src/Screens/Clients.js
@@ -0,0 +1,13 @@
+// import background from "../Images/contact-background.jpeg";
+import Navbar from "../Components/Navbar";
+
+const Clients = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Clients </h1>
+    </>
+  );
+};
+
+export default Clients;
