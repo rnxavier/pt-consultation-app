71c22660cace5da1cda3ad7b8e3ef6bbe1de2487
Richelle Xavier <richellexavier@Richelles-MBP.home>
2022-09-09 11:31:06 +0100

    Moved modal to its own file and added props, update muscle not working

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,34 +1,188 @@
-import { db } from "../Firebase";
-import { useState, useEffect } from "react";
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 
-const Modal = () => {
-  const [clients, setClients] = useState([]);
-  useEffect(() => {
-    db.collection("clients").onSnapshot((snapshot) => {
-      setClients(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          data: doc.data(),
-        }))
-      );
-    });
-  }, []);
-
-  console.log(clients);
+const Modal = (props) => {
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+    documentTitle: "Client Measurements",
+  });
   return (
     <>
-      <div className="modal-background">
-        <div className="modal-container">
-          <button>X</button>
+      <div className="close-btn">
+        <button onClick={props.handleClick}>X</button>
+      </div>
 
-          <div className="modal-title">
-            <h1>Body Measurements</h1>
-          </div>
-
-          <div className="modal-body"></div>
-
-          <div className="modal-footer">BUTTONS</div>
+      <div className="modal-container" ref={componentRef}>
+        <div className="modal-title">
+          <h2>Body Measurements: {props.name}</h2>
         </div>
+
+        <div className="modal-body">
+          <div className="modal-grid centre-txt">
+            <h3>Measurement</h3>
+            <h3>Current Value</h3>
+            <h3>New Value</h3>
+            <h3>Update</h3>
+
+            <p>Height</p>
+            <p>{props.height}</p>
+            <div className="txtb new-measurement-txtb">
+              <input
+                placeholder="Height"
+                type="text"
+                name=""
+                value={props.updatedHeight}
+                onChange={props.onChangeHeight}
+              />
+            </div>
+
+            <button className="update-btn" onClick={props.updateHeight}>
+              +
+            </button>
+
+            <p>Weight</p>
+            <p>{props.weight}</p>
+            <div className="txtb new-measurement-txtb">
+              <input
+                placeholder="Weight"
+                type="text"
+                name=""
+                value={props.updatedWeight}
+                onChange={props.onChangeWeight}
+              />
+            </div>
+            <button className="update-btn" onClick={props.updateWeight}>
+              +
+            </button>
+
+            <p>Body Fat</p>
+            <p>{props.bodyFat}</p>
+            <div className="txtb new-measurement-txtb">
+              <input
+                placeholder="Body Fat"
+                type="text"
+                name=""
+                value={props.updatedBodyFat}
+                onChange={props.onChangeBodyFat}
+              />
+            </div>
+            <button className="update-btn" onClick={props.updateBodyFat}>
+              +
+            </button>
+
+            <p>Water</p>
+            <p>{props.water}</p>
+            <div className="txtb new-measurement-txtb">
+              <input
+                placeholder="Water"
+                type="text"
+                name=""
+                value={props.updatedWater}
+                onChange={props.onChangeWater}
+              />
+            </div>
+            <button className="update-btn" onClick={props.updateWater}>
+              +
+            </button>
+
+            <p>Muscle</p>
+            <p>{props.muscle}</p>
+            <div className="txtb new-measurement-txtb">
+              <input
+                placeholder="Muscle"
+                type="text"
+                name=""
+                value={props.updatedMuscle}
+                onChange={props.onChangeMuscle}
+              />
+            </div>
+            <button className="update-btn" onClick={props.updateMuscle}>
+              +
+            </button>
+
+            <p>Physique</p>
+            <p>{props.physique}</p>
+            <div className="txtb new-measurement-txtb">
+              <input
+                placeholder="Physique"
+                type="text"
+                name=""
+                value={props.updatedPhysique}
+                onChange={props.onChangePhysique}
+              />
+            </div>
+            <button className="update-btn" onClick={props.updatePhysique}>
+              +
+            </button>
+
+            <p>Metabolic Rate</p>
+            <p>{props.metabolicRate}</p>
+            <div className="txtb new-measurement-txtb">
+              <input
+                placeholder="Metabolic Rate"
+                type="text"
+                name=""
+                value={props.updatedMetabolicRate}
+                onChange={props.onChangeMetabolicRate}
+              />
+            </div>
+            <button className="update-btn" onClick={props.updateMetabolicRate}>
+              +
+            </button>
+
+            <p>Metabolic Age</p>
+            <p>{props.metabolicAge}</p>
+            <div className="txtb new-measurement-txtb">
+              <input
+                placeholder="Metabolic Age"
+                type="text"
+                name=""
+                value={props.updatedMetabolicAge}
+                onChange={props.onChangeMetabolicAge}
+              />
+            </div>
+            <button className="update-btn" onClick={props.updateMetabolicAge}>
+              +
+            </button>
+
+            <p>Bone Mass</p>
+            <p>{props.boneMass}</p>
+            <div className="txtb new-measurement-txtb">
+              <input
+                placeholder="Bone Mass"
+                type="text"
+                name=""
+                value={props.updatedBoneMass}
+                onChange={props.onChangeBoneMass}
+              />
+            </div>
+            <button className="update-btn" onClick={props.updateBoneMass}>
+              +
+            </button>
+
+            <p>Visceral Fat</p>
+            <p>{props.visceralFat}</p>
+            <div className="txtb new-measurement-txtb">
+              <input
+                placeholder="Visceral Fat"
+                type="text"
+                name=""
+                value={props.updatedVisceralFat}
+                onChange={props.onChangeVisceralFat}
+              />
+            </div>
+            <button className="update-btn" onClick={props.updateVisceralFat}>
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="single-btn-div">
+        <button className="button" onClick={handlePrint}>
+          <div className="btn-text">Save as PDF</div>
+        </button>
       </div>
     </>
   );
